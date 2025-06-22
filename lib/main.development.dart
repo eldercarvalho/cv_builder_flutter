657f1bff1b75ed2afaea8747aa0e84bf28eafe1d
Elder Carvalho <eld.carv@gmail.com>
2025-06-22 17:35:31 -0300

    feat: implemented notifications

--- lib/main.development.dart
+++ lib/main.development.dart
@@ -5,12 +5,14 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'app.dart';
 import 'config/di.dart';
+import 'data/services/messaging/messaging_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   setupDependencies();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await MessagingService().init();
   await ScreenUtil.ensureScreenSize();
 
   runApp(const CvBuilderApp());
